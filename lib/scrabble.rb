1ab034eda1517269925d731a0148a01c66a2ee32
Katarina Tuttle and Matt Carlson <matt@>
2016-06-28 10:05:29 -0700

    Edit spacing

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,6 +1,5 @@
 class String
   define_method(:scrabble) do
-
     self.tr!('aeioulnrst', '1')
     self.tr!('dg', '2')
     self.tr!('bcmp', '3')
@@ -8,7 +7,6 @@ class String
     self.tr!('k', '5')
     self.tr!('jx', '8')
     self.tr!('qz', 'A')
-
     answer_array = self.split('')
     total = 0
     answer_array.each do |item|
